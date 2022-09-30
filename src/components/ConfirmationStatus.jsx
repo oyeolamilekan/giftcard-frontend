080879e38665ec5a6b42ae7d80573d9318e88a07
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-09-30 16:04:40 +0100

    chore: added ui fix.

--- src/components/ConfirmationStatus.jsx
+++ src/components/ConfirmationStatus.jsx
@@ -1,11 +1,11 @@
 export default function ConfirmationStatus({ state }) {
     switch (state) {
         case "paid":
-            return <div className="alert alert-primary">Payment recieved thanks for shopping with us</div>  
+            return <div className="alert alert-primary">Payment received thanks for shopping with us.</div>  
         case "processing":
-            return <div className="alert alert-primary">Payment has been received and we are confirming from blockchain</div>  
+            return <div className="alert alert-secondary">Payment received and the transaction is currently being processed.</div>  
         case "initiated":
-            return <div className="alert alert-primary">Currently listening to your payment on the blockchain</div>  
+            return <div className="alert alert-primary">Your transaction is currently being processed.</div>  
         default:
             return <></>
     }
